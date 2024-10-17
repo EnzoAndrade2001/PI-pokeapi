f7c09fddca05485600ecd06b32f88931464cc7a8
Enzo de Andrade Cruz <182220019@senacinformatica.br>
2024-10-16 21:39:40 -0300

    alteracao api pokemon

--- src/js/fetchApi/fetchfunctions.js
+++ src/js/fetchApi/fetchfunctions.js
@@ -1,7 +1,25 @@
 import { urlPokeApi } from "../constants/constants.js"
+import { showError } from "../errors/errors.js";
+
 
 export async function listAllPokemons(urlApi = urlPokeApi){
+
+  
+try{
+    const data = await fetch(urlApi);
+
+    const response = await data.json();
+ 
+
+    return response; 
+
+}      catch(error){
+       await showError("Ops um erro inexperado ocorreu ao carregar a lista de pokemons!");
+      
+       }
+
     // try{
+
 
     //     const data = await fetch(urlApi);
     //     const json = await data.json();
