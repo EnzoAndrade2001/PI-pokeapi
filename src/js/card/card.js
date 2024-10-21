6e71b457747a8bbb1889b7a2705407a231e69a65
felipesereno <felipeserenososo@gmail.com>
2024-10-21 16:40:28 -0300

    Primeira versão da janela de detalhes implementada

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -1,21 +1,30 @@
-import { pokemonList } from "../constants/constants.js"
+import { pokemonList } from "../constants/constants.js";
+import { createModal } from "../modal/modal.js"; // Certifique-se de que está importando corretamente
 
-
-
-export function createCard(pokemon, index) {
-
-    console.log(pokemon);
-
+export function createCard(pokemon) {
     const id = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
 
-    const card = `<div class="card" style="width: 18rem;">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${pokemon.name}</h5>
-                        <p class="card-text"></p>
-                        <a href="${pokemon.url}" class="btn btn-primary">Ver mais</a>
-                    </div>
-                </div>`
+    const card = `
+        <div class="card" style="width: 18rem;">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" class="card-img-top" alt="${pokemon.name}">
+            <div class="card-body">
+                <h5 class="card-title">${pokemon.name}</h5>
+                <a href="#" class="btn btn-primary" id="view-more-${id}">Ver mais</a>
+            </div>
+        </div>
+    `;
 
-    pokemonList.innerHTML += card;
+    // Usando insertAdjacentHTML para adicionar o card ao DOM
+    pokemonList.insertAdjacentHTML('beforeend', card);
+
+    // Agora o elemento existe no DOM, podemos adicionar o event listener
+    const viewMoreButton = document.getElementById(`view-more-${id}`);
+    if (viewMoreButton) {
+        viewMoreButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            createModal(pokemon); // Chama a função que abre o modal com os detalhes do Pokémon
+        });
+    } else {
+        console.error(`Botão "Ver mais" para o Pokémon ${pokemon.name} não encontrado.`);
+    }
 }
---
+++ src/js/modal/modal.js
@@ -0,0 +1,32 @@
+export function createModal(pokemon) {
+    const id = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
+
+    // Buscar detalhes do Pokémon na PokeAPI
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(response => response.json())
+        .then(data => {
+            // Preencher os detalhes no modal
+            document.getElementById('pokemon-img').src = data.sprites.front_default;
+            document.getElementById('pokemon-name').textContent = data.name;
+            document.getElementById('pokemon-height').textContent = `${data.height / 10} m`;
+            document.getElementById('pokemon-weight').textContent = `${data.weight / 10} kg`;
+
+            // Tipos do Pokémon
+            const types = data.types.map(typeInfo => typeInfo.type.name).join(', ');
+            document.getElementById('pokemon-types').textContent = types;
+
+            // Habilidades do Pokémon
+            const abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ');
+            document.getElementById('pokemon-abilities').textContent = abilities;
+
+            // Estatísticas do Pokémon
+            const stats = data.stats.map(stat => `<p><strong>${stat.stat.name}:</strong> ${stat.base_stat}</p>`).join('');
+            document.getElementById('pokemon-stats').innerHTML = stats;
+
+            // Inicializar o modal Bootstrap (usando a instância global do Bootstrap já carregada via CDN)
+            const modalElement = document.getElementById('pokemonModal');
+            const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+            modalInstance.show(); // Abre o modal
+        })
+        .catch(error => console.error('Erro ao buscar detalhes do Pokémon:', error));
+}
