7e00d0351f2ff0d468fca0ee25efa1851b4a9bc7
Enzo de Andrade Cruz <182220019@senacinformatica.br>
2024-10-23 21:39:08 -0300

    tentando estilizar o modal

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -11,22 +11,19 @@ export function createModal(pokemon) {
             document.getElementById('pokemon-height').textContent = `${data.height / 10} m`;
             document.getElementById('pokemon-weight').textContent = `${data.weight / 10} kg`;
 
-            // Tipos do Pokémon
             const types = data.types.map(typeInfo => typeInfo.type.name).join(', ');
             document.getElementById('pokemon-types').textContent = types;
 
-            // Habilidades do Pokémon
             const abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ');
             document.getElementById('pokemon-abilities').textContent = abilities;
 
-            // Estatísticas do Pokémon
             const stats = data.stats.map(stat => `<p><strong>${stat.stat.name}:</strong> ${stat.base_stat}</p>`).join('');
             document.getElementById('pokemon-stats').innerHTML = stats;
 
-            // Inicializar o modal Bootstrap (usando a instância global do Bootstrap já carregada via CDN)
+            // Inicializar o modal Bootstrap
             const modalElement = document.getElementById('pokemonModal');
             const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
-            modalInstance.show(); // Abre o modal
+            modalInstance.show(); // Exibir o modal
         })
         .catch(error => console.error('Erro ao buscar detalhes do Pokémon:', error));
 }
