a3de0e26e997c41666f2741405ca1134efa3cda3
felipesereno <felipeserenososo@gmail.com>
2024-10-24 18:06:37 -0300

    Melhoramento no controle do carrossel

--- src/js/main.js
+++ src/js/main.js
@@ -33,22 +33,26 @@ displayAllPokemons();
 // Adiciona evento de busca na barra de pesquisa
 searchInput.addEventListener('input', (e) => {
     const query = e.target.value; // Captura o texto digitado pelo usuário
-    pokemonList.style.animationPlayState = 'paused';
+    //pokemonList.style.animationPlayState = 'paused';
     if (query === '') {
         displayAllPokemons(); // Se a barra estiver vazia, exibe todos os Pokémons
     } else {
         filterPokemons(query); // Caso contrário, filtra os Pokémons
+        pokemonList.style.animationPlayState = 'paused';
     }
 });
 
 // Escurecer o fundo quando a barra de pesquisa estiver em foco
 searchInput.addEventListener('focus', () => {
     body.classList.add('dimmed-background');
+    //pokemonList.style.animation = 'none';
+    pokemonList.style.animationPlayState = 'paused';
 });
 
 // Remover o escurecimento quando a barra de pesquisa perder o foco
 searchInput.addEventListener('blur', () => {
     body.classList.remove('dimmed-background');
+    pokemonList.style.animation = 'scroll 70s linear infinite';
 });
 
 // Carrossel para com mouse em cima
@@ -60,10 +64,20 @@ pokemonList.addEventListener('mouseover', () => {
 // Carrossel retoma com mouse fora
 pokemonList.addEventListener('mouseleave', () => {
     //const carrossel = event.target;
+    pokemonList.style.animation = 'scroll 70s linear infinite';
     pokemonList.style.animationPlayState = 'running';
 });
 
-// Implementar rolagem controlada
-pokemonList.addEventListener('wheel', () => {
-    pokemonList.style.animation = 'scroll 20s linear infinite;';
+//Controle da direção do slide
+pokemonList.addEventListener('wheel', (event) => {
+    event.preventDefault(); // Evita o comportamento padrão de scroll vertical
+
+    if (event.deltaY > 0) {
+        console.log('Rodinha do mouse rolando para baixo'); // Scroll para baixo
+        pokemonList.style.animation = 'scroll 20s linear infinite reverse';
+        
+    } else {
+        console.log('Rodinha do mouse rolando para cima'); // Scroll para cima
+        pokemonList.style.animation = 'scroll 20s linear infinite';
+    }
 });
