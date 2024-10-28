6b7bf0551790daf6981b2f1dd8725be492b982c4
eduarda marranghello <enzoandradecruz10@gmail.com>
2024-10-27 22:26:27 -0300

    add botao e nome centralizado

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -3,16 +3,15 @@ import { createModal } from "../modal/modal.js"; // Certifique-se de que está i
 
 export function createCard(pokemon) {
     const id = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
-
-    // Tornar a primeira letra do nome maiúscula
     const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
     const card = `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 15rem;"> <!-- Diminuindo a largura -->
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" class="card-img-top" alt="${pokemonName}">
-            <div class="card-body">
-                <h5 class="card-title">${pokemonName}</h5>
+            <div class="card-body" style="text-align: center;">
+                <h5 class="card-title pokemon-name">${pokemonName}</h5>
                 <a href="#" class="btn btn-primary" id="view-more-${id}">Ver mais</a>
+                <a href="#" class="btn btn-favorite" id="favorite-${id}">Favoritar</a>
             </div>
         </div>
     `;
@@ -20,14 +19,22 @@ export function createCard(pokemon) {
     // Usando insertAdjacentHTML para adicionar o card ao DOM
     pokemonList.insertAdjacentHTML('beforeend', card);
 
-    // Agora o elemento existe no DOM, podemos adicionar o event listener
+    // Adicionando eventos aos botões
     const viewMoreButton = document.getElementById(`view-more-${id}`);
+    const favoriteButton = document.getElementById(`favorite-${id}`);
+
     if (viewMoreButton) {
         viewMoreButton.addEventListener('click', (event) => {
             event.preventDefault();
-            createModal(pokemon); // Chama a função que abre o modal com os detalhes do Pokémon
+            createModal(pokemon);
         });
-    } else {
-        console.error(`Botão "Ver mais" para o Pokémon ${pokemonName} não encontrado.`);
+    }
+
+    if (favoriteButton) {
+        favoriteButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            alert(`${pokemonName} adicionado aos favoritos!`);
+        });
     }
 }
+
