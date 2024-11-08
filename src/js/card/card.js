b9aa6e26188c4aa15f174f569416142e1407fd8c
Enzo de Andrade Cruz <182220019@senacinformatica.br>
2024-11-07 21:32:37 -0300

    tentando implementar responsividade telas ate 480px

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -26,7 +26,7 @@ export function createCard(pokemon) {
     const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
     const card = `
-        <div class="card" style="width: 15rem; position: relative;">
+        <div class="card card-length" >
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" class="card-img-top" alt="${pokemonName}">
             <div class="card-body" style="text-align: center;">
                 <h5 class="card-title pokemon-name">${pokemonName}</h5>
