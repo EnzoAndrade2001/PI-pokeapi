e051db0aa8353f7b063787eb7d853fb77c854e4d
felipesereno <felipeserenososo@gmail.com>
2024-11-10 20:20:34 -0300

    Favoritos removidos da tela na pesquisa ou filtragem por tipos

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -4,4 +4,4 @@ export const searchInput = document.querySelector('form input');
 export const body = document.querySelector('body');
 export const typeIcons = document.querySelectorAll('.type');
 export const modal = document.getElementById('pokemonModal');
-
+export const favList = document.getElementById("favorites-wrapper");
